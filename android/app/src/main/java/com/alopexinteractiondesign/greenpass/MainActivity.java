cd8ead029e9eed3e34d27b34c35a63dbbfccb9b3
Shaun Bond <shaun@alopexid.com>
2019-03-12 19:16:05 -0800

    Added splash screens.

--- android/app/src/main/java/com/alopexinteractiondesign/greenpass/MainActivity.java
+++ android/app/src/main/java/com/alopexinteractiondesign/greenpass/MainActivity.java
@@ -2,4 +2,11 @@ package com.alopexinteractiondesign.greenpass;
 
 import com.reactnativenavigation.NavigationActivity;
 
-public class MainActivity extends NavigationActivity {}
+public class MainActivity extends NavigationActivity {
+
+  @Override
+  protected void onCreate( android.os.Bundle savedInstanceState ) {
+    setTheme(R.style.AppTheme);
+    super.onCreate(savedInstanceState);
+  }
+}
