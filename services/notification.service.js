fb55fca5638064ece536ede178f5433d0a7405dc
Shaun Bond <dev@alopexid.com>
2019-04-10 21:07:22 -0800

    Fixed iOS build.

--- services/notification.service.js
+++ services/notification.service.js
@@ -90,7 +90,7 @@ export default class NotificationService {
                 exact: true
             })
             
-            setToken()
+            getToken()
         })
         .catch( error => {
             // Don't notify
